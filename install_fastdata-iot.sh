058535388d64fbd2bde6678ee57652be56f3f9f5
Boesman <stompnoe@gmail.com>
2019-02-06 10:16:32 +1100

    update akka-ingest for strict

--- install_fastdata-iot.sh
+++ install_fastdata-iot.sh
@@ -15,19 +15,19 @@ install_fastdata-iot_spark () {
   dcos security org service-accounts keypair spark_priv.pem spark_pub.pem
   dcos security org service-accounts create -p spark_pub.pem -d "Spark service account" spark-iot
   dcos security secrets create-sa-secret --strict spark_priv.pem spark-iot spark/spark-iot
-  sleep 10
+  sleep 2
 
   echo "Permissioning Spark Service Account"
   dcos security org users grant spark-iot dcos:mesos:master:task:user:root create
   dcos security org users grant spark-iot dcos:mesos:master:framework:role:* create
   dcos security org users grant spark-iot dcos:mesos:master:task:app_id:/spark create
   dcos security org users grant dcos_marathon dcos:mesos:master:task:user:root create
-  sleep 10
+  sleep 2
 
   echo "Installing Spark DCOS Package"
   dcos package install spark --options=spark_strict.json --yes
   echo "Waiting for Spark install to finish"
-  sleep 120
+  sleep 90
 }
 
 install_fastdata-iot_cassandra () {
@@ -35,57 +35,57 @@ install_fastdata-iot_cassandra () {
   dcos security org service-accounts keypair cassandra_priv.pem cassandra_pub.pem
   dcos security org service-accounts create -p cassandra_pub.pem -d "Cassandra service account" cassandra-iot
   dcos security secrets create-sa-secret --strict cassandra_priv.pem cassandra-iot cassandra/cassandra-iot
-  sleep 10
+  sleep 2
 
   echo "Permissioning Cassandra Service Account"
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:framework:role:cassandra-role \
   -d '{"description":"Controls the ability of cassandra-role to register as a framework with the Mesos master"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:reservation:role:cassandra-role \
   -d '{"description":"Controls the ability of cassandra-role to reserve resources"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:volume:role:cassandra-role \
   -d '{"description":"Controls the ability of cassandra-role to access volumes"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:reservation:principal:cassandra-iot \
  -d '{"description":"Controls the ability of cassandra-iot to reserve resources"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:volume:principal:cassandra-iot \
   -d '{"description":"Controls the ability of cassandra-iot to access volumes"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:framework:role:cassandra-role/users/cassandra-iot/create
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:reservation:role:cassandra-role/users/cassandra-iot/create
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:volume:role:cassandra-role/users/cassandra-iot/create
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:task:user:nobody/users/cassandra-iot/create
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:reservation:principal:cassandra-iot/users/cassandra-iot/delete
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:volume:principal:cassandra-iot/users/cassandra-iot/delete
-  sleep 10
+  sleep 2
 
   echo "Installing Cassandra DCOS Package"
   dcos package install cassandra --options=cassandra_strict.json --yes
   echo "Waiting for Cassandra DCOS Nodes"
-  sleep 180
+  sleep 150
   dcos job add cassandra-schema.json
   sleep 10
   dcos job run init-cassandra-schema-job
@@ -96,63 +96,63 @@ install_fastdata-iot_kafka () {
   dcos security org service-accounts keypair kafka_priv.pem kafka_pub.pem
   dcos security org service-accounts create -p kafka_pub.pem -d "kafka service account" kafka-iot
   dcos security secrets create-sa-secret --strict kafka_priv.pem kafka-iot kafka/kafka-iot
-  sleep 10
+  sleep 2
 
   echo "Permissioning Kafka Service Account"
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:framework:role:kafka-role \
   -d '{"description":"Controls the ability of kafka-role to register as a framework with the Mesos master"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:reservation:role:kafka-role \
   -d '{"description":"Controls the ability of kafka-role to reserve resources"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:volume:role:kafka-role \
   -d '{"description":"Controls the ability of kafka-role to access volumes"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:reservation:principal:kafka-iot \
   -d '{"description":"Controls the ability of kafka-iot to reserve resources"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:volume:principal:kafka-iot \
   -d '{"description":"Controls the ability of kafka-iot to access volumes"}' \
   -H 'Content-Type: application/json'
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:framework:role:kafka-role/users/kafka-iot/create
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:reservation:role:kafka-role/users/kafka-iot/create
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:volume:role:kafka-role/users/kafka-iot/create
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:task:user:nobody/users/kafka-iot/create
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:reservation:principal:kafka-iot/users/kafka-iot/delete
-  sleep 10
+  sleep 2
   curl -X PUT --cacert dcos-ca.crt \
   -H "Authorization: token=$(dcos config show core.dcos_acs_token)" $(dcos config show core.dcos_url)/acs/api/v1/acls/dcos:mesos:master:volume:principal:kafka-iot/users/kafka-iot/delete
-  sleep 10
+  sleep 2
 
   echo "Installing Kafka DCOS Package"
   dcos package install kafka --options=kafka_strict.json --yes
   echo "Waiting for Kafka Brokers"
-  sleep 120
+  sleep 90
 }
 
 install_fastdata-iot_ingest () {
   echo "Install Ingestion Akka App"
   dcos marathon app add akka-ingest.json
-  sleep 180
+  sleep 120
 }
 
 install_fastdata-iot_spark_job () {
---
+++ main.tf
@@ -0,0 +1,46 @@
+variable "dcos_install_mode" {
+  description = "specifies which type of command to execute. Options: install or upgrade"
+  default     = "install"
+}
+
+variable "dcos_aws_region" {
+  description = "specifies which type of command to execute. Options: install or upgrade"
+  default     = "us-west-2"
+}
+
+data "http" "whatismyip" {
+  url = "http://whatismyip.akamai.com/"
+}
+
+module "dcos" {
+  source = "dcos-terraform/dcos/aws"
+
+  dcos_instance_os    = "rhel_7.3"
+  cluster_name        = "iot-dcos"
+  ssh_public_key_file = "~/.ssh/ccm.pub"
+  admin_ips           = ["${data.http.whatismyip.body}/32"]
+
+  num_masters        = "3"
+  num_private_agents = "4"
+  num_public_agents  = "2"
+
+  dcos_version = "1.12.1"
+  dcos_security = "strict"
+  dcos_variant  = "ee"
+  dcos_license_key_contents = "<insert license key>"
+  # dcos_variant = "open"
+
+  dcos_install_mode = "${var.dcos_install_mode}"
+}
+
+output "masters-ips" {
+  value = "${module.dcos.masters-ips}"
+}
+
+output "cluster-address" {
+  value = "${module.dcos.masters-loadbalancer}"
+}
+
+output "public-agents-loadbalancer" {
+  value = "${module.dcos.public-agents-loadbalancer}"
+}
